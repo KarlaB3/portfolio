9586b272b647e98de739468d13738bfbfe879961
KarlaB3 <k.bucoy@gmail.com>
2023-06-02 22:17:54 +1000

    refactor: project card styling update

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -4,11 +4,11 @@ import { faGithub} from '@fortawesome/free-brands-svg-icons'
 
 export const NavFooter = () => {
     return (
-            <nav class="navbar fixed-bottom navbar-expand-xl navbar-light bg-light">
-                <a class="navbar-brand" target="_blank" href="https://www.linkedin.com/in/karla-bucoy/">
+            <nav className="navbar fixed-bottom navbar-expand-xl navbar-light bg-light">
+                <a className="navbar-brand" target="_blank" href="https://www.linkedin.com/in/karla-bucoy/">
                     <FontAwesomeIcon icon={faLinkedin}  width="80" height="80" alt="LinkedIn profile" />
                 </a>
-                <a class="navbar-brand" target="_blank" href="https://github.com/KarlaB3">
+                <a className="navbar-brand" target="_blank" href="https://github.com/KarlaB3">
                     <FontAwesomeIcon icon={faGithub} width="80" height="80" alt="GitHub profile" />
                 </a>
             </nav>
--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -2,16 +2,16 @@ import logo from './../Images/kb-logo.png';
 
 export const NavHeader = () => {
     return (
-        <div class="container">
-            <nav class="navbar navbar-expand-xl navbar-light bg-light">
-                <a class="navbar-brand" href="/">
+        <div className="container">
+            <nav className="navbar navbar-expand-xl navbar-light bg-light">
+                <a className="navbar-brand" href="/">
                     <img src={logo} width="50" height="50" alt="KB dev logo"></img>
                 </a>
                     <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="/about">About</a>
-                    <a class="nav-item nav-link" href="/projects">Projects</a>
-                    <a class="nav-item nav-link" href="/blogs">Blog</a>
-                    <a class="nav-item nav-link" href="/contact">Contact</a>
+                    <a className="nav-item nav-link" href="/about">About</a>
+                    <a className="nav-item nav-link" href="/projects">Projects</a>
+                    <a className="nav-item nav-link" href="/blogs">Blog</a>
+                    <a className="nav-item nav-link" href="/contact">Contact</a>
                     </div>
             </nav>
         </div>
