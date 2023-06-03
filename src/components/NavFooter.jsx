0305decb1b6584265a75932981663222f9008cac
KarlaB3 <k.bucoy@gmail.com>
2023-06-03 23:11:20 +1000

    refactor: updated target blank link security risk

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -5,10 +5,10 @@ import { faGithub} from '@fortawesome/free-brands-svg-icons'
 export const NavFooter = () => {
     return (
             <nav className="navbar fixed-bottom navbar-expand-xl navbar-light bg-light">
-                <a className="navbar-brand" target="_blank" href="https://www.linkedin.com/in/karla-bucoy/">
+                <a className="navbar-brand" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/karla-bucoy/">
                     <FontAwesomeIcon icon={faLinkedin}  width="80" height="80" alt="LinkedIn profile" />
                 </a>
-                <a className="navbar-brand" target="_blank" href="https://github.com/KarlaB3">
+                <a className="navbar-brand" target="_blank" rel="noreferrer" href="https://github.com/KarlaB3">
                     <FontAwesomeIcon icon={faGithub} width="80" height="80" alt="GitHub profile" />
                 </a>
             </nav>
