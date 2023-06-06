0b6132a833801f6db8aae0724f65ad34f5532b7c
KarlaB3 <k.bucoy@gmail.com>
2023-06-06 20:07:16 +1000

    refactor: home page images and additional sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
-
+import projects from './../Images/projects-img.png'
+import blogs from './../Images/blog-img.png'
+import contact from './../Images/contact-img.png'
 
 export function HomePage(props) {
     return (
         <div>
-            <h1>Hello, I'm Karla</h1>
+            <section>
+                <h1>Hello, I'm Karla</h1>
+                <h4>This website is a work in progress. Stay tuned.</h4>
+            </section>
+            <section>
+                <img className="projects-img" src={projects} width="200" height="200" alt="Projects"/>
+                <img className="blog-img" src={blogs} width="200" height="200" alt="Blog"/>
+                <img className="contact-img" src={contact} width="200" height="200" alt="Contact Form"/>
+            </section>
         </div>
     )
 }
