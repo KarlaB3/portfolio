b12a971ab8613f8f33e29f09ee0d3e4f93e374cd
KarlaB3 <k.bucoy@gmail.com>
2023-06-07 20:08:58 +1000

    refactor: home page images and AboutButton code simplified

--- src/components/AboutButton.jsx
+++ src/components/AboutButton.jsx
@@ -1,26 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 export const AboutButton = () => {
-    const navigate = useNavigate();
-
-    const navigatetoABout = () => {
-        navigate("/about");
-    };
-
-    return (
-        <button onClick={navigatetoABout}>About Me</button>
-    )
+  return (
+    <Link to="/about">
+      <button>
+        About
+      </button>
+    </Link>
+  );
 };
-
-// Alternative code
-// import { Link } from 'react-router-dom';
-
-// export const AboutButton = () => {
-//   return (
-//     <Link to="/about">
-//       <button>
-//         About
-//       </button>
-//     </Link>
-//   );
-// };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import projects from './../Images/projects-img.png'
 import blogs from './../Images/blog-img.png'
-import contact from './../Images/contact-img.png'
+import contact from './../Images/contact-img-2.png'
+import profile from './../Images/profile-img.png'
 import { AboutButton } from '../components/AboutButton'
 
 export function HomePage(props) {
     return (
         <div>
             <section>
-                <h1>Hello, I'm Karla</h1>
                 <h4>This website is a work in progress. Stay tuned.</h4>
             </section>
             <section className="introduction">
@@ -15,6 +15,9 @@ export function HomePage(props) {
                 <h4>Hi, I'm Karla.</h4>
                 <h6>I'm professionally passionate about bringing digital projects and platforms to life, and crafting exceptional user experiences.</h6>
                 <AboutButton />
+            </section>
+            <section>
+                <img className="profile-img" src={profile} width="290" height="300" alt="Profile"/>
             </section>
             <section>
                 <img className="projects-img" src={projects} width="200" height="200" alt="Projects"/>
