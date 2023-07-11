6c5f7df68d9c58a3225635498bc5e8d175cc242a
KarlaB3 <k.bucoy@gmail.com>
2023-07-11 15:09:06 +1000

    refactor: started implementing semantic elements

--- src/components/BlogsCard.jsx
+++ src/components/BlogsCard.jsx
@@ -1,15 +1,19 @@
 export const BlogsCard = ({ blog }) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title mb-3">{blog.title}</h5>
-        <h6 className="card-subtitle mb-3 text-muted">{`#${blog.tags}`}</h6>
-        <p className="card-text">{blog.description}</p>
-        <a href={blog.url}className="card-link" target="_blank" rel="noreferrer" >Read more on dev.to
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z" fill="currentColor"/>
-        </svg></a>
-      </div>
-    </div>
+    <article className="card">
+      <section className="card-body">
+        <header>
+          <h5 className="card-title mb-3">{blog.title}</h5>
+        </header>
+          <section>          
+            <h6 className="card-subtitle mb-3 text-muted">{`#${blog.tags}`}</h6>
+            <p className="card-text">{blog.description}</p>
+            <a href={blog.url}className="card-link" target="_blank" rel="noreferrer" >Read more on dev.to
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z" fill="currentColor"/>
+            </svg></a>
+          </section>
+      </section>
+    </article>
   );
 };
--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,7 +1,7 @@
 export const NavFooter = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light mt-3" id="footer">
-      <div className="navbar-text">
+    <nav className="navbar navbar-expand-lg navbar-light" id="footer">
+      <div className="navbar-text px-4">
         <p>© KB Dev 2023. Illustrations by <a href="https://www.declanmooreart.com/"  id="link" target="_blank" rel="noreferrer">Declan Moore<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z" fill="currentColor"/></svg></a></p>
       </div>
